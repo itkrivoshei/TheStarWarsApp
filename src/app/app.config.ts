9e779cf4155898c6f1b30bb4e96da15f4dc0a284
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-26 20:08:36 +0100

    Refactor app.config.ts to update providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+export const appConfig = {
+  providers: [
+    provideHttpClient(),
+    provideRouter(appRoutes),
+    provideAnimations(),
+  ],
 };
