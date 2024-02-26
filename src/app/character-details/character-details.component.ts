a1c735c6814c7b24ca7fe692abfe4e18ad3e8b5d
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-26 20:09:26 +0100

    Add OnInit interface and implement it in CharacterDetailsComponent

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from '../services/swapi.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './character-details.component.html',
-  styleUrl: './character-details.component.scss'
+  styleUrls: ['./character-details.component.scss'],
 })
-export class CharacterDetailsComponent {
+export class CharacterDetailsComponent implements OnInit {
+  character$: Observable<any> | undefined;
 
+  constructor(
+    private swapiService: SwapiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      if (id !== null) {
+        this.character$ = this.swapiService.getCharacterDetails(id);
+      }
+    });
+  }
 }
