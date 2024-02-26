f29f017c51032dcd1adebf57035d0ec66d183c1b
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-26 20:09:54 +0100

    Add OnInit lifecycle hook and movie loading logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SwapiService } from '../services/swapi.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies: any[] = [];
+  loading: boolean = true;
 
+  constructor(private swapiService: SwapiService) {}
+
+  ngOnInit(): void {
+    this.swapiService.getMovies().subscribe({
+      next: (response) => {
+        this.movies = response.results;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      },
+    });
+  }
 }
