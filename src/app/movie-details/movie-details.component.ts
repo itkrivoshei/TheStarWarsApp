4cdd7d88cbd593430287fc8e0663b096ce774cf4
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-26 20:10:12 +0100

    Add OnInit interface and movie details functionality

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from '../services/swapi.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.scss'
+  styleUrls: ['./movie-details.component.scss'],
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  movie$?: Observable<any>;
+  id: string | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private swapiService: SwapiService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('id');
+      if (this.id) {
+        this.loadMovieDetails(this.id);
+      }
+    });
+  }
+
+  private loadMovieDetails(id: string) {
+    this.movie$ = this.swapiService.getMovieDetails(id);
+  }
+
+  extractId(url: string): string {
+    const matches = url.match(/\/([0-9]+)\/$/);
+    return matches ? matches[1] : '';
+  }
 }
