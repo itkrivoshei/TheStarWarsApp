c92d009542406fc0b3eb5bb119ecc7671976d52d
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-27 22:13:37 +0100

    Remove standalone property from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
