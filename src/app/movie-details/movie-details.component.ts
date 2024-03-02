f26a9b876d1cbd32119441e31df014dfe7d018b6
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-02 18:26:16 +0100

    Refactor movie details component to fetch and display character details

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SwapiService } from '../services/swapi.service';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+interface Character {
+  name: string;
+  id: string;
+}
+
+interface MovieDetails {
+  title: string;
+  director: string;
+  characters: string[];
+}
 
 @Component({
   selector: 'app-movie-details',
@@ -14,6 +26,7 @@ import { RouterModule } from '@angular/router';
 })
 export class MovieDetailsComponent implements OnInit {
   movie$?: Observable<any>;
+  characters: Character[] = [];
   id: string | null = null;
 
   constructor(
@@ -22,16 +35,37 @@ export class MovieDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.id = params.get('id');
-      if (this.id) {
-        this.loadMovieDetails(this.id);
-      }
-    });
-  }
-
-  private loadMovieDetails(id: string) {
-    this.movie$ = this.swapiService.getMovieDetails(id);
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          this.id = params.get('id');
+          if (this.id) {
+            return this.swapiService.getMovieDetails(this.id);
+          }
+          throw new Error('Movie ID not found');
+        }),
+        switchMap((movie: MovieDetails) => {
+          // Fetch all character details in parallel
+          const characterObservables = movie.characters.map((url: string) =>
+            this.swapiService.getCharacterDetails(this.extractId(url))
+          );
+          return forkJoin(characterObservables).pipe(
+            map((characters) =>
+              characters.map((character: any) => ({
+                name: character.name,
+                id: this.extractId(character.url),
+              }))
+            ),
+            map((characterDetails) => ({
+              ...movie,
+              characters: characterDetails,
+            }))
+          );
+        })
+      )
+      .subscribe((movieWithCharacterDetails) => {
+        this.movie$ = of(movieWithCharacterDetails);
+      });
   }
 
   extractId(url: string): string {
