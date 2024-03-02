d02b04b3912d0892b6abc95c2300de36d65f1fc2
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-02 18:25:55 +0100

    Add RouterModule to imports in HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SwapiService } from '../services/swapi.service';
+
+interface Movie {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
@@ -18,7 +36,10 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.swapiService.getMovies().subscribe({
       next: (response) => {
-        this.movies = response.results;
+        this.movies = response.results.map((movie: Movie) => {
+          const id = movie.url.split('/').filter(Boolean).pop();
+          return { ...movie, id };
+        });
         this.loading = false;
       },
       error: () => {
