7546d0ead12b8aac72acfebec90900d621961727
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-02 18:25:40 +0100

    Refactor character-details component to fetch and display film details

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -1,30 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SwapiService } from '../services/swapi.service';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-character-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.scss'],
 })
 export class CharacterDetailsComponent implements OnInit {
-  character$: Observable<any> | undefined;
+  character$?: Observable<any>;
 
   constructor(
-    private swapiService: SwapiService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private swapiService: SwapiService
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      const id = params['id'];
-      if (id !== null) {
-        this.character$ = this.swapiService.getCharacterDetails(id);
-      }
-    });
+    this.character$ = this.route.params.pipe(
+      switchMap((params) => {
+        const id = params['id'];
+        return this.swapiService.getCharacterDetails(id);
+      }),
+      switchMap((character) => {
+        const filmIDs = character.films.map((filmUrl: string) =>
+          this.extractIdFromUrl(filmUrl)
+        );
+        const filmDetailsObservables = filmIDs.map((id: string) =>
+          this.swapiService.getMovieDetails(id)
+        );
+        return forkJoin(filmDetailsObservables).pipe(
+          map((filmDetails) => ({
+            ...character,
+            films: filmDetails,
+          }))
+        );
+      })
+    );
+  }
+
+  private extractIdFromUrl(url: string): string {
+    const matches = url.match(/\/([0-9]+)\/$/);
+    return matches ? matches[1] : '';
   }
 }
