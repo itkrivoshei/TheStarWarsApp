50dbb47ceddb661b2d6dfd6aa82e14e3c985e2ca
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-03 23:40:16 +0100

    Add @ngrx/store dependency

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(appRoutes),
     provideAnimations(),
-  ],
+    provideStore()
+],
 };
