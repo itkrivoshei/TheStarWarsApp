839cd7cb1e8bc85b87ce32cbb7f6b25bef85fcf6
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-04 19:31:19 +0100

    Add @ngrx/store and @ngrx/effects to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { DataEffects } from './store/effects/effects';
+import { reducer as dataReducer } from './store/reducers/reducer';
 
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ data: dataReducer }),
+    EffectsModule.forRoot([DataEffects]),
+  ],
   providers: [],
 })
 export class AppModule {}
