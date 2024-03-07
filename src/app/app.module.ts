f44006a284cc1badd0b94d89dd59dc34f0320982
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-07 22:53:59 +0100

    Add HttpClientModule and update store configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { DataEffects } from './store/effects/effects';
-import { reducer as dataReducer } from './store/reducers/reducer';
+import { charactersReducer } from './shared/store/reducers/characters.reducer';
+import { filmsReducer } from './shared/store/reducers/films.reducer';
+import { DataEffects } from './shared/store/effects/effects';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ data: dataReducer }),
+    HttpClientModule,
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+        },
+      }
+    ),
+    StoreModule.forFeature('characters', charactersReducer),
+    StoreModule.forFeature('films', filmsReducer),
     EffectsModule.forRoot([DataEffects]),
+    CoreModule,
   ],
   providers: [],
+  bootstrap: [],
 })
 export class AppModule {}
