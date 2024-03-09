63d2df0fde2a86b1117defbd6a206bd5fb2a66e0
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-09 19:19:24 +0100

    Update main.ts with Angular dependencies and configuration

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { charactersReducer } from './app/shared/store/reducers/characters.reducer';
+import { filmsReducer } from './app/shared/store/reducers/films.reducer';
+import { DataEffects } from './app/shared/store/effects/effects';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes),
+      StoreModule.forRoot(
+        {
+          characters: charactersReducer,
+          films: filmsReducer,
+        },
+        {
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true,
+            strictStateSerializability: true,
+            strictActionSerializability: true,
+          },
+        }
+      ),
+      EffectsModule.forRoot([DataEffects])
+    ),
+  ],
+}).catch((err) => console.error(err));
