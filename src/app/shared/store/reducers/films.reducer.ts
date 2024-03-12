7f7631d61e6542b02b49caff6e4fc49813d335fa
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-12 15:09:28 +0100

    Add films reducer

---
+++ src/app/shared/store/reducers/films.reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer, on } from '@ngrx/store';
+import * as DataActions from '../actions/actions';
+import { FilmsState, initialFilmsState } from '../state';
+
+export const filmsReducer = createReducer<FilmsState>(
+  initialFilmsState,
+  on(DataActions.loadFilmsSuccess, (state, { films }) => {
+    return Array.isArray(films) && films.length === 1
+      ? { ...state, selected: films[0], error: null }
+      : { ...state, list: films, error: null };
+  }),
+  on(DataActions.loadDataFailure, (state, { error }) => {
+    return { ...state, error };
+  })
+);
