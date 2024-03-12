1bf9f9344428d79de9ed66ef8d84759099522ea1
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-12 14:57:47 +0100

    Update routes to use new component paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,19 +5,19 @@ export const appRoutes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./home/home.component').then((m) => m.HomeComponent),
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'movies/:id',
+    path: 'films/:id',
     loadComponent: () =>
-      import('./movie-details/movie-details.component').then(
-        (m) => m.MovieDetailsComponent
+      import('./pages/film-details/film-details.component').then(
+        (m) => m.FilmDetailsComponent
       ),
   },
   {
     path: 'characters/:id',
     loadComponent: () =>
-      import('./character-details/character-details.component').then(
+      import('./pages/character-details/character-details.component').then(
         (m) => m.CharacterDetailsComponent
       ),
   },
