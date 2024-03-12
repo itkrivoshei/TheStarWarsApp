efa27346d7a5240f9c224ec09f57c2b2c2d2f020
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-12 15:09:25 +0100

    Add characters reducer

---
+++ src/app/shared/store/reducers/characters.reducer.ts
@@ -0,0 +1,21 @@
+import { createReducer, on } from '@ngrx/store';
+import * as DataActions from '../actions/actions';
+import { initialCharactersState } from '../state';
+
+export const charactersReducer = createReducer(
+  initialCharactersState,
+  on(DataActions.loadCharactersSuccess, (state, { characters }) => ({
+    ...state,
+    list: characters,
+    error: null,
+  })),
+  on(DataActions.loadCharacterSuccess, (state, { character }) => ({
+    ...state,
+    selected: character,
+    error: null,
+  })),
+  on(DataActions.loadDataFailure, (state, { error }) => ({
+    ...state,
+    error,
+  }))
+);
