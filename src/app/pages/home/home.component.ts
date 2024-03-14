d7d8d830f28e62f5eac804a48263b3566c501cb0
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-14 12:31:43 +0100

    Add sorting functionality to films observable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { loadData } from '../../shared/store/actions/actions';
 import { AppState } from '../../shared/store/state';
 import { Film } from '../../core/models/film.model';
@@ -19,7 +20,13 @@ export class HomeComponent implements OnInit {
   films$: Observable<Film[]>;
 
   constructor(private store: Store<AppState>) {
-    this.films$ = this.store.select(selectAllFilms);
+    this.films$ = this.store
+      .select(selectAllFilms)
+      .pipe(
+        map((films) =>
+          films.slice().sort((a, b) => a.episode_id - b.episode_id)
+        )
+      );
   }
 
   ngOnInit(): void {
