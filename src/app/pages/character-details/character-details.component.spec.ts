834462ff0b3e3097522a0bbb18c7dabafc21e6de
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-15 13:12:01 +0100

    Refactor character-details.component.spec.ts

--- src/app/pages/character-details/character-details.component.spec.ts
+++ src/app/pages/character-details/character-details.component.spec.ts
@@ -1,22 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import { CharacterDetailsComponent } from './character-details.component';
 
 describe('CharacterDetailsComponent', () => {
-  let component: CharacterDetailsComponent;
-  let fixture: ComponentFixture<CharacterDetailsComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterDetailsComponent],
+      imports: [
+        CharacterDetailsComponent,
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [provideMockStore({})],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(CharacterDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(CharacterDetailsComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
