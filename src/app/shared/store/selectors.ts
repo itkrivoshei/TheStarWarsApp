aa7d2de09ffdf6f2af417fbfc78e6a3eb5d3b2d2
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-20 10:28:48 +0100

    Refactor selectors in shared store

--- src/app/shared/store/selectors.ts
+++ src/app/shared/store/selectors.ts
@@ -1,5 +1,5 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { CharactersState, FilmsState, AppState } from './state';
+import { FilmsState, AppState } from './state';
 
 export const selectCharactersState = createSelector(
   (state: AppState) => state.characters,
@@ -7,32 +7,17 @@ export const selectCharactersState = createSelector(
 );
 export const selectFilmsState = createFeatureSelector<FilmsState>('films');
 
-export const selectAllCharacters = createSelector(
-  (state: AppState) => state.characters,
-  (state: CharactersState) => state.list
-);
-
 export const selectSelectedCharacter = createSelector(
   selectCharactersState,
   (charactersState) => charactersState?.selected
 );
 
-export const selectCharactersError = createSelector(
-  selectCharactersState,
-  (state: CharactersState) => state.error
-);
-
 export const selectAllFilms = createSelector(
   (state: AppState) => state.films,
-  (filmsState: FilmsState) => filmsState.list
+  (filmsState: FilmsState) => (filmsState ? filmsState.list : [])
 );
 
 export const selectSelectedFilm = createSelector(
   selectFilmsState,
   (filmsState: FilmsState) => (filmsState ? filmsState.selected : null)
-);
-
-export const selectFilmsError = createSelector(
-  selectFilmsState,
-  (filmsState: FilmsState) => (filmsState ? filmsState.error : null)
 );
