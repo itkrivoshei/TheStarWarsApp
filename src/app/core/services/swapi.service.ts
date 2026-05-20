f1a11348c7b05a6142749c87adbcd35e602afb81
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-20 14:58:43 +0200

    Tighten SWAPI service types

--- src/app/core/services/swapi.service.ts
+++ src/app/core/services/swapi.service.ts
@@ -1,30 +1,39 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, forkJoin } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, forkJoin, of } from 'rxjs';
+
 import { Character } from '../models/character.model';
 import { Film } from '../models/film.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SwapiService {
-  private baseUrl: string = 'https://swapi.info/api/';
+  private readonly baseUrl = 'https://swapi.info/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
 
   getCharacter(id: string): Observable<Character> {
-    return this.http.get<Character>(`${this.baseUrl}people/${id}/`);
+    return this.http.get<Character>(this.buildUrl('people', id));
   }
 
   getFilm(id: string): Observable<Film> {
-    return this.http.get<Film>(`${this.baseUrl}films/${id}/`);
+    return this.http.get<Film>(this.buildUrl('films', id));
   }
 
-  getFilms(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}films/`);
+  getFilms(): Observable<Film[]> {
+    return this.http.get<Film[]>(this.buildUrl('films'));
   }
 
   getFilmsByUrls(filmUrls: string[]): Observable<Film[]> {
-    const filmObservables = filmUrls.map((url) => this.http.get<Film>(url));
-    return forkJoin(filmObservables);
+    if (!filmUrls.length) {
+      return of([]);
+    }
+
+    return forkJoin(filmUrls.map((url) => this.http.get<Film>(url)));
+  }
+
+  private buildUrl(resource: 'films' | 'people', id?: string): string {
+    return id ? `${this.baseUrl}/${resource}/${id}/` : `${this.baseUrl}/${resource}/`;
   }
 }
