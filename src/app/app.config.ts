80bc0cd2e02f3618a94347757b997b662e07e0c7
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-20 14:51:52 +0200

    Centralize standalone Angular providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,37 @@
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 
-export const appConfig = {
+import { appRoutes } from './app.routes';
+import { charactersReducer } from './shared/store/reducers/characters.reducer';
+import { filmsReducer } from './shared/store/reducers/films.reducer';
+import { DataEffects } from './shared/store/effects/effects';
+
+export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
-    provideRouter(appRoutes),
-    provideAnimations(),
-    provideStore()
-],
+    provideHttpClient(withFetch()),
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
+    provideAnimationsAsync(),
+    provideStore(
+      {
+        characters: charactersReducer,
+        films: filmsReducer,
+      },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+        },
+      }
+    ),
+    provideEffects([DataEffects]),
+  ],
 };
