3df790c115423e834df9189b355997c17054d82b
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-20 14:55:07 +0200

    Optimize home component rendering

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { Film } from '../../core/models/film.model';
 import { loadData } from '../../shared/store/actions/actions';
 import { AppState } from '../../shared/store/state';
-import { Film } from '../../core/models/film.model';
 import { selectAllFilms } from '../../shared/store/selectors';
 
 @Component({
@@ -15,21 +16,25 @@ import { selectAllFilms } from '../../shared/store/selectors';
   imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  films$: Observable<Film[]>;
+  protected readonly films$: Observable<Film[]> = this.store
+    .select(selectAllFilms)
+    .pipe(map((films) => [...films].sort((a, b) => a.episode_id - b.episode_id)));
 
-  constructor(private store: Store<AppState>) {
-    this.films$ = this.store
-      .select(selectAllFilms)
-      .pipe(
-        map((films) =>
-          films.slice().sort((a, b) => a.episode_id - b.episode_id)
-        )
-      );
-  }
+  constructor(private readonly store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(loadData({ apiType: 'film', id: '' }));
+  }
+
+  protected trackByEpisodeId(_: number, film: Film): number {
+    return film.episode_id;
+  }
+
+  protected getFilmResourceId(url: string): string {
+    const match = /\/films\/(\d+)\/?$/.exec(url);
+    return match?.[1] ?? '';
   }
 }
