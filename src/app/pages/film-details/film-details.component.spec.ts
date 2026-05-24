874b5a7430978de3dad915d1b612e33e9e60c1c9
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 16:56:01 +0200

    test: tidy film details spec imports

--- src/app/pages/film-details/film-details.component.spec.ts
+++ src/app/pages/film-details/film-details.component.spec.ts
@@ -1,7 +1,8 @@
-import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+
 import { FilmDetailsComponent } from './film-details.component';
 
 describe('FilmDetailsComponent', () => {
@@ -19,6 +20,7 @@ describe('FilmDetailsComponent', () => {
   it('should create', () => {
     const fixture = TestBed.createComponent(FilmDetailsComponent);
     const component = fixture.componentInstance;
+
     expect(component).toBeTruthy();
   });
 });
